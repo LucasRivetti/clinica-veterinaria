2ff3f7d4e490acbec0a72d2f01bb1806422a4ec0
Lucas Rivetti <lucassantosrivetti@gmail.com>
2025-04-16 16:31:04 -0300

    create class entities (Entidades)

--- src/clinicaVeterinaria/modelo/Entidade.java
+++ src/clinicaVeterinaria/modelo/Entidade.java
@@ -1,5 +1,23 @@
 package clinicaVeterinaria.modelo;
+public abstract class Entidade {
+    protected int id;
 
-public class Entidade {
+    public Entidade(int id) {
+        this.id = id;
+    }
     
+    public Entidade() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Id=" + id;
+    }
 }
