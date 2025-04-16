c736c1f6aad133db6870c2ef9fe1a04bab98363b
Lucas Rivetti <lucassantosrivetti@gmail.com>
2025-04-16 17:24:18 -0300

    Create Class Client

---
+++ src/clinicaVeterinaria/modelo/Cliente.java
@@ -0,0 +1,72 @@
+package clinicaVeterinaria.modelo;
+
+public class Cliente extends Entidade {
+    private String nome;
+    private String telefone;
+    private String email;
+    private String cpf;
+    private String endereco;
+
+    public Cliente(int id, String nome, String telefone, String email, String cpf, String endereco) {
+        super(id);
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public Cliente() {}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente[" +
+                "id=" + getId() +
+                ", nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ']';
+    }
+}
