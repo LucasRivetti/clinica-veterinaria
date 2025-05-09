837d9d9edc9785c61b42e9353f9ae5b5df5fa68f
Augusto Cezar <augustoccfaria@gmail.com>
2025-05-09 17:11:43 -0300

    criada a classe banco de dados

--- src/clinicaVeterinaria/persistencia/BancoDeDados.java
+++ src/clinicaVeterinaria/persistencia/BancoDeDados.java
@@ -1,5 +1,36 @@
 package clinicaVeterinaria.persistencia;
 
+import clinicaVeterinaria.modelo.Veterinario;
+import clinicaVeterinaria.modelo.Cliente;
+import clinicaVeterinaria.modelo.Animal;
+import clinicaVeterinaria.modelo.Consulta;
+
 public class BancoDeDados {
-    
+    private Persistente<Veterinario> veterinarios;
+    private Persistente<Cliente> clientes;
+    private Persistente<Animal> animais;
+    private Persistente<Consulta> consultas;
+
+    public BancoDeDados() {
+        this.veterinarios = new Persistente<>();
+        this.clientes = new Persistente<>();
+        this.animais = new Persistente<>();
+        this.consultas = new Persistente<>();
+    }
+
+    public Persistente<Veterinario> getVeterinarios() {
+        return veterinarios;
+    }
+
+    public Persistente<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public Persistente<Animal> getAnimais() {
+        return animais;
+    }
+
+    public Persistente<Consulta> getConsultas() {
+        return consultas;
+    }
 }
