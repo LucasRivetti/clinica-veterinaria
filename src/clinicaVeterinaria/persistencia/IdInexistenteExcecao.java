d016b4fd98645666a06713817114fd27c6e1afbd
Augusto Cezar <augustoccfaria@gmail.com>
2025-05-09 16:22:48 -0300

    Classe de persistencia foi criada e a classe para tratamento de erro de ids

---
+++ src/clinicaVeterinaria/persistencia/IdInexistenteExcecao.java
@@ -0,0 +1,8 @@
+public class IdInexistenteExcecao extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public IdInexistenteExcecao(String mensagem) {
+        super(mensagem);
+    }
+    
+}
--- src/clinicaVeterinaria/persistencia/Persistente.java
+++ src/clinicaVeterinaria/persistencia/Persistente.java
@@ -1,5 +1,54 @@
 package clinicaVeterinaria.persistencia;
 
-public class Persistente {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+import clinicaVeterinaria.modelo.Entidade;
+
+public class Persistente<T extends Entidade> {
+    private List<T> listaDeEntidades; //Atributo para armazenar os listaDeEntidades
+
+    public Persistente( ) {
+        this.listaDeEntidades = new ArrayList<>();
+    }
+
+    public void adicionar(T entidade) {
+        this.listaDeEntidades.add(entidade);
+    }
+
+    public void remover(int id) throws IdInexistenteExcecao {
+        T entidade = buscarPorId(id);
+        this.listaDeEntidades.remove(entidade);
+    }
+
+   public void atualizar(T entidade) {
+        for (int i = 0; i < listaDeEntidades.size(); i++) {
+            if (listaDeEntidades.get(i).getId() == entidade.getId()) {
+                listaDeEntidades.set(i, entidade);
+                break;
+            }
+        }
+    }
+
+    public T buscarPorId(int id) throws IdInexistenteExcecao {
+        for (T entidade : listaDeEntidades) {
+            if (entidade.getId() == id) {
+                return entidade;
+            }
+        }
+        throw new IdInexistenteExcecao("ID não encontrado: " + id);
+    }
+
+     public List<T> listar() {
+        return this.listaDeEntidades;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T entidade : listaDeEntidades) {
+            sb.append(entidade.toString()).append("\n");
+        }
+        return sb.toString();
+    }
 }
