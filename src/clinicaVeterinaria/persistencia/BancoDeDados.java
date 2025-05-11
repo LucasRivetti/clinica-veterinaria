2e2978ed76c221a398ae160de939678b59c50c55
Lucas Rivetti <lucassantosrivetti@gmail.com>
2025-05-11 16:02:32 -0300

    Adicao dos Procedimentos ao banco de dados

--- src/clinicaVeterinaria/persistencia/BancoDeDados.java
+++ src/clinicaVeterinaria/persistencia/BancoDeDados.java
@@ -1,36 +1,30 @@
 package clinicaVeterinaria.persistencia;
 
-import clinicaVeterinaria.modelo.Veterinario;
-import clinicaVeterinaria.modelo.Cliente;
-import clinicaVeterinaria.modelo.Animal;
-import clinicaVeterinaria.modelo.Consulta;
+import clinicaVeterinaria.modelo.*;
 
 public class BancoDeDados {
-    private Persistente<Veterinario> veterinarios;
     private Persistente<Cliente> clientes;
+    private Persistente<Veterinario> veterinarios;
     private Persistente<Animal> animais;
     private Persistente<Consulta> consultas;
+    private Persistente<Procedimento> procedimentos;
 
     public BancoDeDados() {
-        this.veterinarios = new Persistente<>();
         this.clientes = new Persistente<>();
+        this.veterinarios = new Persistente<>();
         this.animais = new Persistente<>();
         this.consultas = new Persistente<>();
+        this.procedimentos = new Persistente<>();
     }
 
-    public Persistente<Veterinario> getVeterinarios() {
-        return veterinarios;
+    public Persistente<Cliente> getClientes() { 
+        return clientes; }
+    public Persistente<Veterinario> getVeterinarios() { 
+        return veterinarios; }
+    public Persistente<Animal> getAnimais() { 
+        return animais; }
+    public Persistente<Consulta> getConsultas() { 
+        return consultas; }
+    public Persistente<Procedimento> getProcedimentos() { 
+        return procedimentos; }
     }
-
-    public Persistente<Cliente> getClientes() {
-        return clientes;
-    }
-
-    public Persistente<Animal> getAnimais() {
-        return animais;
-    }
-
-    public Persistente<Consulta> getConsultas() {
-        return consultas;
-    }
-}
